fd56ffae716bc32566d825f4c9884e521e31e004
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-04-10 04:04:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+function introduction(name) {
+  console.log(`Hi, my name is ${name}`);
+}
+introduction("mohammed");
+
+
+
+
+
+function introductionWithLanguage(userName,  language) {
+  console.log(`Hi, my name is ${userName} and i am learning  ${language}`);
+}
+introductionWithLanguage("mohammed" , "english");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
